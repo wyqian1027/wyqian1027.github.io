9b079a6a30a8d9907c513005109ac23e9f435e83
Wenyang Qian <wyqian1027@gmail.com>
2019-04-04 04:45:37 +0000

    demo

---
+++ ml/assets/js/frozenlake.js
@@ -0,0 +1,20 @@
+var myVideo = document.getElementById("demo"); 
+
+function playPause() { 
+  if (myVideo.paused) 
+    myVideo.play(); 
+  else 
+    myVideo.pause(); 
+} 
+
+function makeBig() { 
+    myVideo.width = 560; 
+} 
+
+function makeSmall() { 
+    myVideo.width = 320; 
+} 
+
+function makeNormal() { 
+    myVideo.width = 420; 
+} 
