020e05919ab3298039c18cfcecc0f8635f6220e4
Wenyang Qian <wyqian1027@gmail.com>
2021-03-24 17:49:56 -0500

    update

--- code/leetcode/probs/713/code-python.py
+++ code/leetcode/probs/713/code-python.py
@@ -1,17 +1,31 @@
+# O(N) sliding window
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        
-        if k <= 1: return 0
-        
+        if k <= 1: return 0 # must take care 1 otherwise pain
         cur = 1
         left = 0
         total = 0
-        
         for right, val in enumerate(nums):
             cur = cur*val
             while cur >= k:
                 cur //= nums[left]
                 left += 1
             total += right - left + 1
-        
         return total
+
+# O(NlgN) convert to subarraySum, with binary search to find pivot pos
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k == 0: return 0
+        k = math.log(k)
+        prefix = [0]
+        for num in nums:
+            prefix.append(math.log(num) + prefix[-1])
+        ans = 0
+        for i, a in enumerate(prefix):
+            pos = bisect.bisect_left(prefix, a-k+1e-9, 0, i)
+            ans += i - pos
+            # checking sum afterwards
+            # pos = bisect.bisect(prefix, a+k-1e-9, i+1)
+            # ans += pos - i - 1
+        return ans
--- code/leetcode/probs/82/code-python.py
+++ code/leetcode/probs/82/code-python.py
@@ -1,15 +1,10 @@
 class Solution:
-
-    def deleteDuplicates(self, head: ListNode) -> ListNode:
-        
+    def deleteDuplicates(self, head: ListNode) -> ListNode:        
         if not head or not head.next: return head
-        
         dummy = ListNode(head.val-1)
         dummy.next = head
         rollback = pre = last = dummy
-        
         while head:
-
             if head.val == last.val:
                 pre = rollback
                 pre.next = None
@@ -17,8 +12,23 @@ class Solution:
                 pre.next = head
                 rollback = pre
                 pre = pre.next
-                
             last = head
             head = head.next
-        
+        return dummy.next
+
+# with less pointers
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        dummy = prev = ListNode(-101, head)
+        cur = head
+        while cur:
+            link = True
+            while cur.next and cur.val == cur.next.val:
+                cur = cur.next
+                link = False
+            if link: 
+                prev.next = cur
+                prev = cur
+            cur = cur.next
+        prev.next = None
         return dummy.next
